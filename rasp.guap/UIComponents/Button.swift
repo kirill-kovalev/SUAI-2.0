734d3adad8cc61d111997ed56cd704a7ab86e29c
Kovalev Kirill <kirilkovalev@ya.ru>
2020-08-05 15:35:41 +0300

    Button with custom target handler; No need to use obj-c functions, target as a closure

---
+++ rasp.guap/UIComponents/Button.swift
@@ -0,0 +1,21 @@
+//
+//  Button.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 31.07.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class Button: UIButton {
+
+    private var actionHandler:(UIButton)->Void = {_ in}
+    func addTarget(action: @escaping (UIButton)->Void, for controlEvents: UIControl.Event) {
+        self.actionHandler = action
+        super.addTarget(self, action: #selector(self.targetAction), for: controlEvents)
+    }
+    @objc private func targetAction(){
+        actionHandler(self)
+    }
+}
