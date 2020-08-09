0762f9532c46c4f912db2c280a9d71b235e54372
Kovalev Kirill <kirilkovalev@ya.ru>
2020-08-09 15:28:25 +0300

    PocketDayView

---
+++ rasp.guap/UIComponents/PocketDay/PocketDayView.swift
@@ -0,0 +1,15 @@
+//
+//  PocketDayView.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 09.08.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class PocketDayView: View {
+
+    @IBOutlet weak var label: UILabel!
+    
+}
