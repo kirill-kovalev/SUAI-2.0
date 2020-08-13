10857388dd8c0818d3e769123ffb725c96541148
Kovalev Kirill <kirilkovalev@ya.ru>
2020-08-13 18:49:43 +0300

    Full New timetable View Controller Based on VStack

--- rasp.guap/Modules/Main App/TabBarPages/ScheduleTabPage/TimetableController/TimetableLessonCell.swift
+++ rasp.guap/Modules/Main App/TabBarPages/ScheduleTabPage/TimetableController/TimetableLessonCell.swift
@@ -8,32 +8,71 @@
 
 import UIKit
 
-class TimetableLessonCell: UITableViewCell{
+
+class TimetableLessonCell: UIButton{
     
     var pocketLessonView:PocketDayView = PocketDayView.fromNib()
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style:style,reuseIdentifier:reuseIdentifier)
-        self.backgroundColor = .clear
-        self.contentView.addSubview(pocketLessonView)
-        pocketLessonView.snp.makeConstraints { (make) in
-            make.top.left.bottom.right.equalToSuperview()
+    var lesson:Timetable.Lesson? = nil
+    var delegate:TimetableLessonCellDelegate? = nil
+    init(lesson:Timetable.Lesson? = nil ) {
+        super.init(frame:.zero)
+
+        self.addSubview(pocketLessonView)
+        self.pocketLessonView.snp.makeConstraints { (make) in
+            make.top.bottom.equalToSuperview()
+            make.left.right.equalToSuperview()
+        }
+        
+        
+//        self.addTarget(self, action: #selector(touchDown(_:)), for: .touchDown)
+//        self.addTarget(self, action: #selector(touchUpInside(_:)), for: .touchUpInside)
+
+        if lesson != nil {
+            setLesson(lesson: lesson!)
         }
     }
+    
     func setLesson(lesson:Timetable.Lesson) {
-        let _ = self.pocketLessonView
+        self.lesson = lesson
+        pocketLessonView.prep.text = lesson.prepods[0].Name
+        pocketLessonView.lessonNum.text = "\(lesson.lessonNum)"
+        pocketLessonView.title.text = lesson.name
+        
+        let tag = PocketTagButton()
+        tag.setTitle("\(lesson.type.rawValue)", for: .normal)
+        tag.isEnabled = false
+        pocketLessonView.tagStack.addArrangedSubview(tag)
+        
+        
+        let groupTag = PocketTagButton()
+        if lesson.groups.count > 1 {
+            groupTag.setTitle("\(lesson.groups.count) группы", for: .normal)
+        }else{
+            groupTag.setTitle(lesson.groups[0].Name, for: .normal)
+        }
+        groupTag.isEnabled = false
+        pocketLessonView.tagStack.addArrangedSubview(groupTag)
     }
+    
+//    @objc private func touchDown(_ sender:UIButton){
+//        UIView.animate(withDuration: 0.15) {
+//            self.backgroundColor = Asset.PocketColors.pocketLightShadow.color
+//        }
+//    }
+//    @objc private func touchUpInside(_ sender:UIButton){
+//        UIView.animate(withDuration: 0.15) {
+//            self.backgroundColor = .clear
+//        }
+//        self.delegate?.didSelect(lesson: self.lesson!)
+//    }
+
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        // Configure the view for the selected state
-    }
-
 }
+protocol TimetableLessonCellDelegate {
+    func didSelect(lesson:Timetable.Lesson)
+}
+
--- rasp.guap/Modules/Main App/TabBarPages/ScheduleTabPage/TimetableController/TimetableViewController.swift
+++ rasp.guap/Modules/Main App/TabBarPages/ScheduleTabPage/TimetableController/TimetableViewController.swift
@@ -8,54 +8,50 @@
 
 import UIKit
 
-class TimetableViewController: UITableViewController {
-    private let lessonCellID = "lessonCell"
+class TimetableViewController: UIViewController, TimetableLessonCellDelegate {
+    func didSelect(lesson: Timetable.Lesson) {
+        let modal = LessonInfoModalViewController()
+        self.present(modal, animated: true, completion:nil)
+    }
     
     let timetable:[Timetable.Lesson]
     private var contentHeight:CGFloat = 0
-    init(timetable:[Timetable.Lesson]) {
-        self.timetable = timetable
-        super.init(style: .plain)
+    let tableView = UIStackView(frame: .zero)
+    
+    init(timetable:[Timetable.Lesson]? = nil) {
+        self.timetable = timetable ?? []
+        super.init(nibName: nil, bundle: nil)
+        self.view = tableView
+        tableView.axis = .vertical
+        tableView.layer.cornerRadius = 10
+        tableView.spacing = 0
+        
+        if timetable != nil {
+            self.setTimetable(timetable: timetable!)
+        }
     }
+    func setTimetable(timetable new:[Timetable.Lesson]){
+        clearStack()
+        for lesson in new {
+            let newView = TimetableLessonCell(lesson: lesson)
+            newView.delegate = self
+            self.tableView.addArrangedSubview(newView)
+        }
+    }
+    private func clearStack(){
+        for view in self.tableView.arrangedSubviews{
+            view.removeConstraints(view.constraints)
+            view.removeFromSuperview()
+        }
+    }
+    @objc func openModal(_ sender:UITapGestureRecognizer,lesson:Int){
+        let modal = LessonInfoModalViewController()
+        self.present(modal, animated: true, completion: nil)
+    }
+    
     
     required init?(coder: NSCoder) {
         self.timetable = []
-        super.init(coder: coder)
-    }
-    
-    override func viewDidLoad() {
-        tableView.separatorStyle = .none
-        tableView.backgroundColor = .clear
-        tableView.layer.cornerRadius = 10
-        tableView.layer.masksToBounds = true
-        tableView.register(TimetableLessonCell.self, forCellReuseIdentifier: lessonCellID)
-        //tableView.reloadData()
-    }
-    override func viewDidAppear(_ animated: Bool) {
-        tableView.snp.makeConstraints { (make) in
-            make.height.equalTo(contentHeight)
-        }
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.lessonCellID , for: indexPath) as! TimetableLessonCell
-        let i = indexPath.row
-        cell.pocketLessonView.prep.text = timetable[i].prepods[0].Name
-        cell.pocketLessonView.lessonNum.text = "\(timetable[i].lessonNum)"
-        cell.pocketLessonView.title.text = timetable[i].name
-        self.contentHeight += cell.frame.height*2 - 5
-        return cell
-        
-    }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return timetable.count
-    }
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.tableView.deselectRow(at: indexPath, animated: true)
-        let modal = LessonInfoModalViewController()
-        self.present(modal, animated: true, completion: {
-            self.tableView.deselectRow(at: indexPath, animated: true)
-        })
+        super.init(nibName: nil, bundle: nil)
     }
 }
-
