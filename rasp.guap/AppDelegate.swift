653c5619de873fa5e86b394c530ad10c0156562a
Kovalev Kirill <kirilkovalev@ya.ru>
2020-08-13 21:32:31 +0300

    demo

--- rasp.guap/AppDelegate.swift
+++ rasp.guap/AppDelegate.swift
@@ -20,8 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         //
-        startApp()
-        //showTutorialPages()
+        //startApp()
+        showTutorialPages()
         return true
     }
     
