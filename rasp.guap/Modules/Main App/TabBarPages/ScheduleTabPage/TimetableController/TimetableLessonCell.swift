bbb9945ebd9a159c9e9270c13d471d98917eb3b7
Kovalev Kirill <kirilkovalev@ya.ru>
2020-08-13 20:23:15 +0300

    timetable cell

--- rasp.guap/Modules/Main App/TabBarPages/ScheduleTabPage/TimetableController/TimetableLessonCell.swift
+++ rasp.guap/Modules/Main App/TabBarPages/ScheduleTabPage/TimetableController/TimetableLessonCell.swift
@@ -38,20 +38,52 @@ class TimetableLessonCell: UIButton{
         pocketLessonView.lessonNum.text = "\(lesson.lessonNum)"
         pocketLessonView.title.text = lesson.name
         
+        addTag(text: lesson.type.rawValue)
+        addTag(text: lesson.groups.count > 1 ? "\(lesson.groups.count) группы" : lesson.groups[0].Name)
+        
+        var counter = 0
+        var place = ""
+        for tag in lesson.tags{
+            if counter<2{
+                place += tag
+            }
+            counter += 1
+        }
+        addTag(text: place)
+        
+        switch lesson.type {
+        case .courseProject:
+            pocketLessonView.verticalLine.backgroundColor = Asset.PocketColors.pocketPurple.color
+            break;
+        case .lab:
+            pocketLessonView.verticalLine.backgroundColor = Asset.PocketColors.pocketGreen.color
+            break
+        case .lecture:
+            pocketLessonView.verticalLine.backgroundColor = Asset.PocketColors.pocketAqua.color
+            break
+            
+        case .practice:
+            pocketLessonView.verticalLine.backgroundColor = Asset.PocketColors.pocketOrange.color
+            break
+
+        }
+       
+        func hf(_ int:Int?)->String{
+             return String(format: "%.2i",  int ?? 0)
+        }
+        
+        pocketLessonView.startTime.text = "\(hf(lesson.startTime.hour)):\(hf(lesson.startTime.minute!))"
+        pocketLessonView.endTime.text = "\(hf(lesson.endTime.hour!)):\(hf(lesson.endTime.minute!))"
+
+        
+        
+        
+    }
+    private func addTag(text: String){
         let tag = PocketTagButton()
-        tag.setTitle("\(lesson.type.rawValue)", for: .normal)
+        tag.setTitle(text, for: .normal)
         tag.isEnabled = false
         pocketLessonView.tagStack.addArrangedSubview(tag)
-        
-        
-        let groupTag = PocketTagButton()
-        if lesson.groups.count > 1 {
-            groupTag.setTitle("\(lesson.groups.count) группы", for: .normal)
-        }else{
-            groupTag.setTitle(lesson.groups[0].Name, for: .normal)
-        }
-        groupTag.isEnabled = false
-        pocketLessonView.tagStack.addArrangedSubview(groupTag)
     }
     
 //    @objc private func touchDown(_ sender:UIButton){
