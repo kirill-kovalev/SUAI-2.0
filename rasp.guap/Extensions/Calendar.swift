c8df537b4d407cbc0090e836992977f922f1eac6
Kovalev Kirill <kirilkovalev@ya.ru>
2020-08-16 18:38:24 +0300

    calendar extension

---
+++ rasp.guap/Extensions/Calendar.swift
@@ -0,0 +1,26 @@
+//
+//  Calendar.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 16.08.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import Foundation
+
+extension Calendar{
+    static func convertToRU(_ day:Int)->Int{
+        let d = day - 1
+        return d < 0 ? 6 : d
+    }
+    static func convertToUS(_ day:Int)->Int{
+        let d = day + 1
+        return d > 6 ? 0 : d
+    }
+    
+    var weekdaysRu:[String] {
+        var symbols = Array(self.weekdaySymbols.dropFirst())
+        symbols.append(self.weekdaySymbols.first!)
+        return symbols
+    }
+}
