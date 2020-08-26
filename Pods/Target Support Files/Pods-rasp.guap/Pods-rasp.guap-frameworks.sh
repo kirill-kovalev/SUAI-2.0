0a3c322ee48c804914c3bd841c6125e83a43817e
Kovalev Kirill <kirilkovalev@ya.ru>
2020-08-27 01:16:48 +0300

    Developer Account Changed

--- Pods/Target Support Files/Pods-rasp.guap/Pods-rasp.guap-frameworks.sh
+++ Pods/Target Support Files/Pods-rasp.guap/Pods-rasp.guap-frameworks.sh
@@ -199,10 +199,12 @@ fi
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ESTabBarController-swift/ESTabBarController_swift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyVK/SwiftyVK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/ESTabBarController-swift/ESTabBarController_swift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyVK/SwiftyVK.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
