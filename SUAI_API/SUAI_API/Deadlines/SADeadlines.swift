5b7d36f5d653993c96471cb195b4954b73c2fffb
Kovalev Kirill <kirilkovalev@ya.ru>
2020-08-31 23:20:21 +0300

    API Update

--- SUAI_API/SUAI_API/Deadlines/SADeadlines.swift
+++ SUAI_API/SUAI_API/Deadlines/SADeadlines.swift
@@ -7,65 +7,6 @@
 //
 
 import Foundation
-public enum SADeadlineGroup{
-	case nearest
-	case open
-	case closed
-}
-
-public struct SADeadline: Codable {
-	public let id: Int
-	public let subject_name: String?
-	public let deadline_name: String?
-	public let closed: Int
-	public let start: Date
-	public let end: Date
-	public let comment: String
-	
-	public var lesson: String {
-		return ""
-	}
-	public init(id: Int = 0,
-				subject_name: String? = "",
-				deadline_name: String = "",
-				closed: Int = 0,
-				start: Date = Date(),
-				end: Date = Date(),
-				comment: String = ""
-	){
-		self.id = id
-		self.subject_name = subject_name
-		self.deadline_name = deadline_name
-		self.closed = closed
-		self.start = start
-		self.end = end
-		self.comment = comment
-	}
-	public var startDate:String{
-		return SADeadline.formatter.string(from: self.start)
-	}
-	public var endDate:String{
-		return SADeadline.formatter.string(from: self.end)
-	}
-	public static let formatter:DateFormatter = {
-		let formatter = DateFormatter()
-		formatter.dateFormat = "yyyy-MM-dd"
-		return formatter
-	}()
-	
-	public var type: SADeadlineGroup{
-		if self.closed == 1{
-			return .closed
-		}else{
-			if self.end < Date().addingTimeInterval(60*60*24*7){
-				return .nearest
-			}else{
-				return .open
-			}
-		}
-	}
-}
-
 
 public class SADeadlines{
 	public static let shared = SADeadlines()
