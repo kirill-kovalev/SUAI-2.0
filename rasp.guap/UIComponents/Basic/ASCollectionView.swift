85b292de892b0c08fd89d94e3cf386638f0b7bdf
Kovalev Kirill <kirilkovalev@ya.ru>
2020-08-31 12:55:39 +0300

    Autosize/ CollextionView

---
+++ rasp.guap/UIComponents/Basic/ASCollectionView.swift
@@ -0,0 +1,24 @@
+//
+//  ASCollectionView.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 26.08.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class ASCollectionView: UICollectionView {
+
+    override var contentSize: CGSize {
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    override var intrinsicContentSize: CGSize {
+        layoutIfNeeded()
+        return CGSize(width: UIView.noIntrinsicMetric, height: contentSize.height)
+    }
+}
+
