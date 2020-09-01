56154135d8f638eb9a91cd5c244e810da79ea89d
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-01 22:28:23 +0300

    some views for brief page

---
+++ rasp.guap/Modules/Main App/TabBarPages/FeedTabPage/BriefInfo/FeedBriefViews.swift
@@ -0,0 +1,21 @@
+//
+//  FeedBriefViws.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 01.09.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class GroupFollowView:UIView{
+    
+}
+
+class WeatherView: UIView {
+    
+}
+
+class RocketsView:UIView{
+    
+}
