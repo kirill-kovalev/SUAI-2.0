56e4a59ffff487aca35a4106c81993c59ca128e3
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-02 23:36:35 +0300

    PocketAvatar

---
+++ rasp.guap/UIComponents/PocketAvatar/PocketAvatar.swift
@@ -0,0 +1,42 @@
+//
+//  PocketAvatar.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 02.09.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class PocketUserAvatar:UIView{
+    var color:UIColor = .red
+    let imageView:UIImageView = {
+        let img = UIImageView(frame: .zero)
+        img.layer.masksToBounds = true
+        img.image = Asset.AppImages.photoPlaceholder.image
+        return img
+    }()
+    init() {
+        super.init(frame: .zero)
+        self.addSubview(imageView)
+        self.layer.borderColor = self.color.cgColor
+        self.layer.borderWidth = 2
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = self.bounds.height/2
+        let size = self.bounds.size
+        let inset:CGFloat = 5
+        self.imageView.frame = CGRect(x: inset, y: inset, width: size.width - inset*2, height: size.height - inset*2)
+        self.imageView.layer.cornerRadius = self.imageView.bounds.size.height/2
+        
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        self.layer.borderColor = self.color.cgColor
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
