c01375061b70d749904d278acc1457a767774f0e
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-03 00:06:14 +0300

    rocket Userlist

--- rasp.guap/Modules/Main App/TabBarPages/FeedTabPage/BriefInfo/RocketModal/RocketModalView.swift
+++ rasp.guap/Modules/Main App/TabBarPages/FeedTabPage/BriefInfo/RocketModal/RocketModalView.swift
@@ -9,5 +9,101 @@
 import UIKit
 
 class RocketModalView: ModalView {
+    let image:UIImageView = {
+        let img = UIImageView(frame: .zero)
+        img.image = Asset.AppImages.stickers.image
+        return img
+    }()
+    let title:UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "Собирай рокеты!"
+        label.font = FontFamily.TTCommons.bold.font(size: 21)
+        label.textColor = Asset.PocketColors.pocketBlack.color
+        return label
+    }()
+    let subtitle:UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "Пользуйся сервисом и получай рокеты! Лучших ждут\n стикеры еженедельно!"
+        label.font = FontFamily.SFProDisplay.regular.font(size: 14)
+        label.textColor = Asset.PocketColors.pocketGray.color
+        return label
+    }()
+    let topTitle:UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = "Топ-3 недели"
+        label.font = FontFamily.SFProDisplay.semibold.font(size: 14)
+        label.textColor = Asset.PocketColors.pocketBlack.color
+        return label
+    }()
+    let stack:UIStackView = {
+        let stack = UIStackView(frame: .zero)
+        stack.axis = .horizontal
+        stack.spacing = 10
+        return stack
+    }()
+    let nextGiveaway:UILabel = {
+        let label = UILabel(frame: .zero)
+        label.text = ""
+        label.font = FontFamily.TTCommons.bold.font(size: 21)
+        label.textColor = Asset.PocketColors.pocketBlack.color
+        return label
+    }()
+    required init() {
+        super.init()
+        self.backgroundColor = .clear
+        addViews()
+        setupConstraints()
+    }
+    
+    func addViews(){
+        addSubview(image)
+        addSubview(title)
+        addSubview(subtitle)
+        addSubview(topTitle)
+        addSubview(stack)
+        addSubview(nextGiveaway)
+    }
+    func setupConstraints(){
+        image.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.size.equalTo(CGSize(width: 128, height: 128))
+        }
+        title.snp.makeConstraints { (make) in
+            make.top.equalTo(image.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.left.greaterThanOrEqualToSuperview().offset(10)
+            make.right.lessThanOrEqualToSuperview().inset(10)
+        }
+        subtitle.snp.makeConstraints { (make) in
+            make.top.equalTo(title.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.left.greaterThanOrEqualToSuperview().offset(10)
+            make.right.lessThanOrEqualToSuperview().inset(10)
+        }
+        topTitle.snp.makeConstraints { (make) in
+            make.top.equalTo(subtitle.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.left.greaterThanOrEqualToSuperview().offset(10)
+            make.right.lessThanOrEqualToSuperview().inset(10)
+        }
+        stack.snp.makeConstraints { (make) in
+            make.top.equalTo(topTitle.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.left.greaterThanOrEqualToSuperview().offset(10)
+            make.right.lessThanOrEqualToSuperview().inset(10)
+        }
+        nextGiveaway.snp.makeConstraints { (make) in
+            make.top.equalTo(stack.snp.bottom)
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.left.greaterThanOrEqualToSuperview().offset(10)
+            make.right.lessThanOrEqualToSuperview().inset(10)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
 }
--- rasp.guap/Modules/Main App/TabBarPages/FeedTabPage/BriefInfo/RocketModal/RocketUserView.swift
+++ rasp.guap/Modules/Main App/TabBarPages/FeedTabPage/BriefInfo/RocketModal/RocketUserView.swift
@@ -14,12 +14,14 @@ class RocketUserView:UIView{
         let label = UILabel(frame: .zero)
         label.font = FontFamily.SFProDisplay.regular.font(size: 14)
         label.textColor = Asset.PocketColors.pocketBlack.color
+        label.textAlignment = .center
         return label
     }()
     let rocketCount:UILabel = {
         let label = UILabel(frame: .zero)
         label.font = FontFamily.SFProDisplay.regular.font(size: 12)
         label.textColor = Asset.PocketColors.pocketGray.color
+        label.textAlignment = .right
         return label
     }()
     let rocketImage:UIImageView = {
@@ -41,18 +43,54 @@ class RocketUserView:UIView{
         username.snp.makeConstraints { (make) in
             make.left.greaterThanOrEqualToSuperview()
             make.right.lessThanOrEqualToSuperview()
+            make.centerX.equalToSuperview()
             make.top.equalTo(avatar.snp.bottom).offset(6)
         }
         rocketCount.snp.makeConstraints { (make) in
             make.left.greaterThanOrEqualToSuperview()
-            make.right.lessThanOrEqualToSuperview()
             make.top.equalTo(username.snp.bottom).offset(6)
+            make.bottom.equalToSuperview()
+            make.centerX.lessThanOrEqualTo(self.snp.centerX)
         }
         rocketImage.snp.makeConstraints { (make) in
             make.centerY.equalTo(rocketCount)
+            make.size.equalTo(CGSize(width: 16, height: 16))
             make.left.equalTo(rocketCount.snp.right).offset(4)
+            make.centerX.greaterThanOrEqualTo(self.snp.centerX)
+            make.right.lessThanOrEqualToSuperview()
         }
     }
+    
+    func setPlace(_ place:Int){
+        let avatarColors = [
+            Asset.PocketColors.pocketYellow.color,
+            Asset.PocketColors.pocketLightGray.color,
+            Asset.PocketColors.pocketOrange.color
+        ]
+        if place >= 0 && place < avatarColors.count{
+            avatar.color = avatarColors[place]
+        }
+        
+    }
+    func setPhoto(url:String){
+        NetworkManager.dataTask(url: url) { (result) in
+            switch result{
+                case .success(let data):
+                    guard let img = UIImage(data: data) else {return}
+                    self.avatar.imageView.image = img
+                    break;
+                case .failure: break
+            }
+        }
+    }
+    func setName(first:String,last:String?=nil){
+        self.username.text = first + " " + (last ?? "")
+    }
+    func  setRockets(count:Int) {
+        self.rocketCount.text = "\(count)"
+    }
+    
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
