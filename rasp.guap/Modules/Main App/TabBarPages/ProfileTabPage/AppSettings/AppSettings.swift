e2f23872c4dcde4155ac81bd0c9e9595870d9081
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-03 13:19:13 +0300

    renamed

--- rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AppSettings/AppSettings.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AppSettings.swift
-//  rasp.guap
-//
-//  Created by Кирилл on 03.09.2020.
-//  Copyright © 2020 Kovalev K.A. All rights reserved.
-//
-
-import Foundation
---
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AppSettings/AppSettingsView.swift
@@ -0,0 +1,19 @@
+//
+//  AppSettingsView.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 03.09.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class AppSettingsView: View {
+	required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+	required init() {
+		super.init()
+	}
+
+}
