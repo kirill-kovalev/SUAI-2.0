3299b417156a4a98220c061f9e79b34b196ec975
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-03 19:02:08 +0300

    AppSettings view

--- rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AppSettings/AppSettingsView.swift
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AppSettings/AppSettingsView.swift
@@ -7,13 +7,83 @@
 //
 
 import Foundation
-
 import UIKit
+import MBRadioButton
 
 class AppSettingsView: View {
 	required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+	func labelGenerator(title:String)->UILabel{
+        let label = UILabel(frame: .zero)
+        label.font = FontFamily.SFProDisplay.regular.font(size: 14)
+        label.textColor = Asset.PocketColors.pocketBlack.color
+        label.text = title
+        return label
+    }
+	lazy var groupLabel = self.labelGenerator(title:"Группа")
+	lazy var startscreenLabel = self.labelGenerator(title:"Начальный экран")
+	lazy var buildingLabel = self.labelGenerator(title:"Корпус по умолчанию")
+	
+	lazy var groupSelector:UITextField = {
+		let tf = PocketTextField(frame: .zero)
+		
+		return tf
+	}()
+	
+	lazy var startScreenSelector:UIStackView = {
+		let stack = UIStackView(frame: .zero)
+		stack.backgroundColor = .red
+		stack.spacing = 2
+		stack.axis = .vertical
+		return stack
+	}()
+	
+	lazy var buildingSelector:UIStackView = {
+		let stack = UIStackView(frame: .zero)
+		stack.backgroundColor = .red
+		stack.spacing = 2
+		stack.axis = .vertical
+		return stack
+	}()
+		
+	
+	
 	required init() {
 		super.init()
+		addViews()
+		setupConstraints()
 	}
-
+	func addViews(){
+		self.addSubview(groupLabel)
+		self.addSubview(groupSelector)
+		self.addSubview(startscreenLabel)
+		self.addSubview(startScreenSelector)
+		self.addSubview(buildingLabel)
+		self.addSubview(buildingSelector)
+		
+	}
+	func setupConstraints(){
+		groupLabel.snp.makeConstraints { $0.top.left.equalToSuperview() }
+		groupSelector.snp.makeConstraints { (make) in
+			make.left.right.equalToSuperview()
+			make.top.equalTo(groupLabel.snp.bottom)
+		}
+		startscreenLabel.snp.makeConstraints { (make) in
+			make.left.right.equalToSuperview()
+			make.top.equalTo(groupSelector.snp.bottom)
+		}
+		startScreenSelector.snp.makeConstraints { (make) in
+			make.left.right.equalToSuperview()
+			make.top.equalTo(startscreenLabel.snp.bottom)
+		}
+		buildingLabel.snp.makeConstraints { (make) in
+			make.left.right.equalToSuperview()
+			make.top.equalTo(startScreenSelector.snp.bottom)
+		}
+		buildingSelector.snp.makeConstraints { (make) in
+			make.left.right.equalToSuperview()
+			make.top.equalTo(buildingLabel.snp.bottom)
+			make.bottom.equalToSuperview()
+		}
+				
+	}
 }
--- rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AppSettings/AppSettingsViewController.swift
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AppSettings/AppSettingsViewController.swift
@@ -6,4 +6,74 @@
 //  Copyright © 2020 Kovalev K.A. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MBRadioButton
+
+
+class AppSettingsViewController: ViewController<AppSettingsView> {
+	
+	let startScreens:[String] = [
+		"Новости",
+		"Дедлайны",
+		"Расписание",
+		"Настройки"
+	]
+	let buildings:[String] = [
+		"Новости",
+		"Дедлайны",
+		"Расписание",
+		"Настройки"
+	]
+
+	let screensRadioContainer = MBRadioButtonContainer([])
+	let buildingsRadioContainer = MBRadioButtonContainer([])
+
+	
+	override func viewDidLoad() {
+		self.keyboardReflective = false
+		setupStartScreens()
+		setupBuildingList()
+	}
+	
+	func setupStartScreens(){
+		let buttons = self.startScreens.enumerated().map{ (offset,title) -> MBRadioButton in
+			let button = MBRadioButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+			button.tag = offset
+			button.setTitle(title, for: .normal)
+			button.setTitleColor(Asset.PocketColors.pocketGray.color, for: .normal)
+			button.titleLabel?.font = FontFamily.SFProDisplay.regular.font(size: 14)
+			self.rootView.startScreenSelector.addArrangedSubview(button)
+			button.snp.makeConstraints { $0.height.equalTo(30)}
+			return button
+		}
+		screensRadioContainer.addButtons(buttons)
+		screensRadioContainer.delegate = self
+	}
+	
+	func setupBuildingList(){
+		let buttons = self.buildings.enumerated().map{ (offset,title) -> MBRadioButton in
+			let button = MBRadioButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+			button.tag = offset
+			button.setTitle(title, for: .normal)
+			button.setTitleColor(Asset.PocketColors.pocketGray.color, for: .normal)
+			button.titleLabel?.font = FontFamily.SFProDisplay.regular.font(size: 14)
+			self.rootView.buildingSelector.addArrangedSubview(button)
+//			button.snp.makeConstraints { $0.height.equalTo(30)}
+			return button
+		}
+		buildingsRadioContainer.addButtons(buttons)
+		buildingsRadioContainer.delegate = self
+	}
+}
+extension AppSettingsViewController:MBRadioButtonDelegate{
+	func radioButtonDidSelect(_ button: MBRadioButton) {
+		
+	}
+	
+	func radioButtonDidDeselect(_ button: MBRadioButton) {
+		
+	}
+	
+	
+}
+
