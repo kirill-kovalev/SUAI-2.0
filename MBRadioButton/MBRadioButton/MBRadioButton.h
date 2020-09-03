e561fd421e0eac75ba71ac5ec97b0736fa030220
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-03 17:39:29 +0300

    custom fork of MBRadioButton

---
+++ MBRadioButton/MBRadioButton/MBRadioButton.h
@@ -0,0 +1,19 @@
+//
+//  MBRadioButton.h
+//  MBRadioButton
+//
+//  Created by Кирилл on 03.09.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MBRadioButton.
+FOUNDATION_EXPORT double MBRadioButtonVersionNumber;
+
+//! Project version string for MBRadioButton.
+FOUNDATION_EXPORT const unsigned char MBRadioButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBRadioButton/PublicHeader.h>
+
+
