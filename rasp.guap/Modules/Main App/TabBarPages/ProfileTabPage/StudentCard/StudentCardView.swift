b7d1de2f29a9e24842c113904e84beae0cff6f2b
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-03 14:57:28 +0300

    PocketTextField

--- rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/StudentCard/StudentCardView.swift
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/StudentCard/StudentCardView.swift
@@ -6,4 +6,72 @@
 //  Copyright © 2020 Kovalev K.A. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class StudentCardView: View {
+	let avatar = PocketUserAvatar()
+	let name:UILabel = {
+		let label = UILabel(frame: .zero)
+		label.font = FontFamily.SFProDisplay.semibold.font(size: 14)
+		label.textColor = Asset.PocketColors.pocketBlack.color
+		return label
+	}()
+	let group:UILabel = {
+		let label = UILabel(frame: .zero)
+		label.font = FontFamily.SFProDisplay.regular.font(size: 14)
+		label.textColor = Asset.PocketColors.pocketGray.color
+		return label
+	}()
+	let suaiImage:UIImageView = {
+		let image = UIImageView(image: Asset.AppImages.suaiCard.image.withRenderingMode(.alwaysTemplate))
+		image.tintColor = Asset.PocketColors.pocketDarkBlue.color
+		return image
+	}()
+	let suai:UILabel = {
+		let label = UILabel(frame: .zero)
+		label.text = "SUAI ID"
+		label.font = FontFamily.SFProDisplay.semibold.font(size: 14)
+		label.textColor = Asset.PocketColors.pocketDarkBlue.color
+		return label
+	}()
+	
+	required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+	required init() {
+		super.init()
+		addViews()
+		setupConstraints()
+		avatar.color = .clear
+	}
+	
+	func addViews(){
+		self.addSubview(avatar)
+		self.addSubview(name)
+		self.addSubview(group)
+		self.addSubview(suaiImage)
+		self.addSubview(suai)
+	}
+	func setupConstraints(){
+		avatar.snp.makeConstraints { (make) in
+			make.top.left.bottom.equalToSuperview()
+			make.size.equalTo(CGSize(width: 60, height: 60))
+		}
+		name.snp.makeConstraints { (make) in
+			make.bottom.equalTo(avatar.snp.centerY)
+			make.left.equalTo(avatar.snp.right)
+		}
+		group.snp.makeConstraints { (make) in
+			make.top.equalTo(avatar.snp.centerY)
+			make.left.equalTo(avatar.snp.right)
+		}
+		suai.snp.makeConstraints { (make) in
+			make.right.equalToSuperview()
+			make.centerY.equalToSuperview()
+		}
+		suaiImage.snp.makeConstraints { (make) in
+			make.right.equalTo(suai.snp.left)
+			make.size.equalTo(CGSize(width: 24, height: 24))
+			make.centerY.equalTo(suai)
+		}
+	}
+
+}
---
+++ rasp.guap/UIComponents/PocketTextField/PocketTextField.swift
@@ -0,0 +1,39 @@
+//
+//  PocketTextField.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 03.09.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class PocketTextField:UITextField{
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupView()
+	}
+	
+	required init?(coder: NSCoder) {
+		super.init(coder:coder)
+		setupView()
+	}
+	
+	func setupView(){
+		self.font = FontFamily.SFProDisplay.regular.font(size: 14)
+		self.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: 12))
+		self.leftViewMode = .always
+		self.layer.cornerRadius = 10
+		self.backgroundColor = Asset.PocketColors.pocketLightGray.color
+		self.textRect(forBounds: self.bounds.insetBy(dx: 5, dy: 5))
+		self.doneAccessory = true
+		traitCollectionDidChange(nil)
+	}
+	
+	override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+		super.traitCollectionDidChange(previousTraitCollection)
+		self.layer.borderColor = Asset.PocketColors.pocketLightGray.color.cgColor
+		self.layer.borderWidth = 1
+	}
+	
+}
