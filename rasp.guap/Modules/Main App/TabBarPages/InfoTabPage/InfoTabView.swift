754d0a3d68eff66b6fb4d31a087adf900b673076
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-04 14:30:45 +0300

    Temporary info page

--- rasp.guap/Modules/Main App/TabBarPages/InfoTabPage/InfoTabView.swift
+++ rasp.guap/Modules/Main App/TabBarPages/InfoTabPage/InfoTabView.swift
@@ -7,23 +7,39 @@
 //
 
 import UIKit
+import WebKit
 
 
 class InfoTabView:TabBarPageView {
-//    let webView:UIWebView = {
-//        let view = UIWebView(frame: .zero)
-//        view.loadRequest(URLRequest(url: URL(string: "https://ya.ru")!))
-//        return view
-//    }()
+    let webView:WKWebView = {
+        let view = WKWebView(frame: .zero)
+		let color = CIColor(color: Asset.PocketColors.pocketWhite.color)
+		view.allowsLinkPreview = false
+		view.evaluateJavaScript("document.body.style.backgroundColor = 'rgb(\(Int(color.red*255)),\(Int(color.green*255)),\(Int(color.blue*255))'", completionHandler: nil)
+        return view
+    }()
+	let indicator:UIActivityIndicatorView = {
+		let indicator = UIActivityIndicatorView(frame: .zero)
+		indicator.hidesWhenStopped = true
+		indicator.startAnimating()
+		return indicator
+	}()
     
     required init() {
         super.init()
-        //super.addSubview(webView)
-//        webView.snp.makeConstraints { (make) in
-//            make.top.equalTo(self.header.snp.bottom)
-//            make.left.right.equalToSuperview()
-//            make.height.equalToSuperview()
-//        }
+        super.addSubview(webView)
+		super.addSubview(indicator)
+		title.snp.makeConstraints { $0.bottom.equalToSuperview()}
+		header.backgroundColor = .clear
+		self.bringSubviewToFront(self.header)
+        webView.snp.makeConstraints { (make) in
+            //make.top.equalTo(self.header.snp.bottom)
+			make.top.equalTo(self)
+            make.left.right.equalToSuperview()
+            make.height.equalToSuperview()
+        }
+		indicator.snp.makeConstraints{$0.center.equalToSuperview()}
+		
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
--- rasp.guap/Modules/Main App/TabBarPages/InfoTabPage/InfoTabViewController.swift
+++ rasp.guap/Modules/Main App/TabBarPages/InfoTabPage/InfoTabViewController.swift
@@ -7,20 +7,32 @@
 //
 
 import UIKit
+import WebKit
 import ESTabBarController_swift
 
 class InfoTabViewController: ViewController<InfoTabView> {
     required init() {
         super.init()
         self.tabBarItem = ESTabBarItem(PocketTabBarIcon(),title: "Справочник", image: Asset.AppImages.TabBarImages.info.image, tag: 3)
-        
+		self.keyboardReflective = false
     }
     
     override func viewDidLoad() {
         self.rootView.setTitle(self.tabBarItem.title ?? "")
-        
+		self.rootView.webView.navigationDelegate = self
+		self.rootView.webView.load(URLRequest(url: URL(string: "http://sputnik.guap.ru/nav/")!))
+		
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
+}
+extension InfoTabViewController:WKNavigationDelegate{
+	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+		self.rootView.indicator.stopAnimating()
+		webView.evaluateJavaScript("document.getElementById('block_menu').remove()", completionHandler: nil)
+	}
+	func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+		print("failed nav \(navigation) wit err \(error)")
+	}
 }
