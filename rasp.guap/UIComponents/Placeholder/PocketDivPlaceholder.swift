bab752b8a288f2500343cc4fba376c10bcfef9df
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-08 19:40:49 +0300

    placeholder

---
+++ rasp.guap/UIComponents/Placeholder/PocketDivPlaceholder.swift
@@ -0,0 +1,13 @@
+//
+//  PocketDivPlaceholder.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 08.09.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class PocketDivPlaceholder:UIView{
+	
+}
