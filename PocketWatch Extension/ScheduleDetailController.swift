2bf81cf48738486d688e11f45cd968b805392c40
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-12 19:40:58 +0300

    Watch app

---
+++ PocketWatch Extension/ScheduleDetailController.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleDetailController.swift
+//  SUAIWatch WatchKit Extension
+//
+//  Created by Кирилл on 12.09.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class ScheduleDetailController: WKInterfaceController {
+	@IBOutlet weak var lessonNameLabel: WKInterfaceLabel!
+	@IBOutlet weak var TimeLabel: WKInterfaceLabel!
+	@IBOutlet weak var PrepLabel: WKInterfaceLabel!
+	@IBOutlet weak var roomsLabel: WKInterfaceLabel!
+	
+}
