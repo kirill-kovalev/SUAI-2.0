0ca6b787229db2bb4708939098dad58cf4feba38
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-14 20:25:45 +0300

    selector update

--- rasp.guap/UIComponents/SwitchSelector/SwitchSelectorView.swift
+++ rasp.guap/UIComponents/SwitchSelector/SwitchSelectorView.swift
@@ -79,8 +79,11 @@ public class SwitchSelector: UIScrollView {
         button.setTitle(element.title, for: .normal)
         button.addTarget(action: { (sender) in
 			if self.feedback { UIImpactFeedbackGenerator(style: .light).impactOccurred() }
-            self.selectedIndex = index
-            self.switchDelegate?.didSelect(index)
+			if self.selectedIndex != index {
+				self.selectedIndex = index
+			    self.switchDelegate?.didSelect(index)
+			}
+           
         }, for: .touchUpInside)
         self.stack.addArrangedSubview(button)
     }
