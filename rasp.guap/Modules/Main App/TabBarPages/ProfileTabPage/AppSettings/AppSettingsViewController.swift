8505502e06a243bf0191d2cd7b28cb7fbd8608c1
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-19 20:21:39 +0300

    fixes

--- rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AppSettings/AppSettingsViewController.swift
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AppSettings/AppSettingsViewController.swift
@@ -19,7 +19,7 @@ class AppSettingsViewController: ViewController<AppSettingsView> {
 		"Новости",
 		"Дедлайны",
 		"Расписание",
-		"Инфо"
+		"Справочник"
 	]
 	// MARK: - Список зданий
 	let buildings:[String] = [
