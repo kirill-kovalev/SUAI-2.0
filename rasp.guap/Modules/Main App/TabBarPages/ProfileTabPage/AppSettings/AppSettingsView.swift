5629a51f6d4ce9391b7734c943fb43ced9b3bfe3
Kovalev Kirill <kirilkovalev@ya.ru>
2020-09-28 13:34:18 +0300

    AppSettings

---
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AppSettings/AppSettingsView.swift
@@ -0,0 +1,68 @@
+//
+//  AppSettingsView.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 28.09.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class AppSettingsView:View{
+	
+	lazy var content:UIStackView = {
+		let stack = UIStackView(frame: .zero)
+		stack.axis = .vertical
+		stack.spacing = 12
+		return stack
+	}()
+	
+	lazy var deadlineNotifications = AppSettingsBlock(title: "Уведомления об открытых дедлайнах")
+	lazy var timetableNotifications = AppSettingsBlock(title: "Уведомления о занятиях")
+	
+	required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+	required init() {
+		super.init()
+		addViews()
+		setupConstraints()
+	}
+	func addViews(){
+		self.addSubview(content)
+		content.addArrangedSubview(deadlineNotifications)
+		content.addArrangedSubview(timetableNotifications)
+	}
+	
+	func setupConstraints(){
+		content.snp.makeConstraints { (make) in
+			make.top.left.right.bottom.equalToSuperview()
+		}				
+	}
+}
+
+class AppSettingsBlock :UIStackView{
+	
+	func labelGenerator(title:String)->UILabel{
+        let label = UILabel(frame: .zero)
+        label.font = FontFamily.SFProDisplay.regular.font(size: 14)
+        label.textColor = Asset.PocketColors.pocketBlack.color
+        label.text = title
+        return label
+    }
+	let toggle:UISwitch = {
+		let view = UISwitch(frame: .zero)
+		view.onTintColor = Asset.PocketColors.accent.color
+		return view
+	}()
+	
+	lazy var label = labelGenerator(title: "")
+	
+	required init(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+	init(title:String) {
+		super.init(frame:.zero)
+		self.axis = .horizontal
+		self.distribution = .equalCentering
+		self.addArrangedSubview(label)
+		self.addArrangedSubview(toggle)
+		label.text = title
+	}
+}
---
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AppSettings/AppSettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AppSettingsViewController.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 28.09.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class AppSettingsViewController :ViewController<AppSettingsView>{
+	
+}
