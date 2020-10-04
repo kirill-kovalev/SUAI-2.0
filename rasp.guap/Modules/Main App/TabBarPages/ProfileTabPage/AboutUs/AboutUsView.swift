26776e8e0f3d717d199db8d3f96837acda6c1d56
Kovalev Kirill <kirilkovalev@ya.ru>
2020-10-04 16:54:22 +0300

    AboutUs Block

--- rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AboutUs/AboutUsView.swift
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AboutUs/AboutUsView.swift
@@ -9,5 +9,29 @@
 import UIKit
 
 class AboutUsView:View{
+	let image:UIImageView = {
+		let view = UIImageView(frame: .zero)
+		view.layer.masksToBounds = true
+		view.layer.cornerRadius = 10
+		view.image = UIImage(named: "AppIconLight")
+		return view
+	}()
+	lazy var imageContainer = PocketScalableContainer(content: image)
+	required init() {
+		super.init()
+		addViews()
+		setupConstraints()
+	}
+	func addViews(){
+		self.addSubview(imageContainer)
+		
+	}
+	func setupConstraints(){
+		imageContainer.snp.makeConstraints { (make) in
+			make.top.left.bottom.equalToSuperview()
+			make.width.height.equalTo(55)
+		}
+	}
 	
+	required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 }
--- rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AboutUs/AboutUsViewController.swift
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AboutUs/AboutUsViewController.swift
@@ -6,8 +6,44 @@
 //  Copyright © 2020 Kovalev K.A. All rights reserved.
 //
 
+//AppIconLight
+
 import UIKit
 
 class AboutUsViewController:ViewController<AboutUsView>{
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		updateViewImage()
+		
+		self.rootView.imageContainer.addTarget(action: { (_) in
+			if UIApplication.shared.supportsAlternateIcons {
+				Logger.print("Supports alternate icons")
+				self.switchIcon()
+			}else{
+				Logger.print("Not supports alternate icon")
+			}
+		}, for: .touchUpInside)
+	}
 	
+	func updateViewImage(){
+		if UIApplication.shared.supportsAlternateIcons,
+		   let iconName = UIApplication.shared.alternateIconName{
+			self.rootView.image.image = UIImage(named: iconName)
+		}
+	}
+	
+	func switchIcon(){
+		if UIApplication.shared.alternateIconName == nil {
+			UIApplication.shared.setAlternateIconName("AppIconDark") { (err) in
+				Logger.print(err)
+				self.updateViewImage()
+			}
+		}else{
+			UIApplication.shared.setAlternateIconName(nil) { (err) in
+				Logger.print(err)
+				self.updateViewImage()
+			}
+		}
+	}
 }
