ded8dd5f152727dd6758015737ba12de4e029ac1
Kovalev Kirill <kirilkovalev@ya.ru>
2020-10-04 20:04:37 +0300

    FeedOrder Modal ViewController

---
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/FeedOrder/FeedOrderSettingsView.swift
@@ -0,0 +1,13 @@
+//
+//  FeedOrderSettingsView.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 04.10.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class FeedOrderSettingsView : ModalView{
+	
+}
