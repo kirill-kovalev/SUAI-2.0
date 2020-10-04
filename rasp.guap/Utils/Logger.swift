2dec362f15cc65f105447f4f65f68e88729eac69
Kovalev Kirill <kirilkovalev@ya.ru>
2020-10-04 13:19:10 +0300

    Logger fixes

--- rasp.guap/Utils/Logger.swift
+++ rasp.guap/Utils/Logger.swift
@@ -9,22 +9,28 @@
 import Foundation
 
 class Logger{
-	public static var log:String = ""
+	public static var log:String = UserDefaults.standard.string(forKey: "Logger.log") ?? ""
 	static func print(_ items:Any?...){
 		
 		let formatter = DateFormatter()
 		formatter.locale = Locale(identifier: "Ru")
-		formatter.dateFormat = ""
+		formatter.dateFormat = "YY/M/dd hh:mm:ss"
 		
 		let date = formatter.string(from: Date())
-		let string = "\n[\(date)] : \(items) \n\n___________________________________________________________________________________________\n\n"
+		let string = "LOGGER - [\(date)] : \(items) \n____________________________________________________________________________________________________\n\n"
 		log.append(string)
-		debugPrint(string)
+		prnt(log: string)
+		UserDefaults.standard.synchronize()
+		UserDefaults.standard.setValue(self.log, forKey: "Logger.log")
 	}
 	static func print(from:Any,_ items:Any?...){
 		Logger.print("\(from.self) \n \(items)")
 	}
 }
-
+fileprivate func prnt(log:Any?){
+	if let log = log{
+		print(log)
+	}
+}
 
 
