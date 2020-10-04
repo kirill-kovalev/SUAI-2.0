30fa671058f1829e3cac7dfc523b71e001f0c0b9
Kovalev Kirill <kirilkovalev@ya.ru>
2020-10-04 14:49:15 +0300

    dropdown TExtField

---
+++ rasp.guap/UIComponents/PocketDropDownField/PocketDropdownField.swift
@@ -0,0 +1,36 @@
+//
+//  PocketDropdownField.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 04.10.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class PocketDropdownView:UIView{
+	let image = UIImageView(image: Asset.SystemIcons.searchDropdown.image.withRenderingMode(.alwaysTemplate))
+	init() {
+		super.init(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+		
+		image.tintColor = Asset.PocketColors.pocketGray.color
+		self.addSubview(image)
+		image.snp.makeConstraints { make in
+			make.width.height.equalTo(30)
+			make.centerY.equalToSuperview()
+			make.right.equalToSuperview().inset(8)
+		}
+	}
+	
+	required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
+
+class PocketDropdownField: PocketTextField {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		self.rightViewMode = .always
+		self.rightView = PocketDropdownView()
+	}
+	
+	required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
