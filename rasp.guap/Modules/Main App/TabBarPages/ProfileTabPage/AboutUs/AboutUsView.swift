1c5eed8b44dbc73c04af928f09cc540c4ea689d0
Kovalev Kirill <kirilkovalev@ya.ru>
2020-10-04 16:21:28 +0300

    aboutUs block

---
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/AboutUs/AboutUsView.swift
@@ -0,0 +1,13 @@
+//
+//  AboutUsView.swift
+//  rasp.guap
+//
+//  Created by Кирилл on 04.10.2020.
+//  Copyright © 2020 Kovalev K.A. All rights reserved.
+//
+
+import UIKit
+
+class AboutUsView:View{
+	
+}
