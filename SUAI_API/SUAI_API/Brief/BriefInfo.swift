e3b9839a39f141e6f9dbb4ac320e26491703c212
Kovalev Kirill <kirilkovalev@ya.ru>
2020-10-04 16:18:20 +0300

    warning shutdown

--- SUAI_API/SUAI_API/Brief/BriefInfo.swift
+++ SUAI_API/SUAI_API/Brief/BriefInfo.swift
@@ -53,15 +53,6 @@ public class SABrief{
 	
 	
 	private func decodeNews(from:[VKFeedElement]) -> [SAFeedElement]{
-		let sources:[FeedSource] = SANews.shared.sources.isEmpty ? [
-			FeedSource(name: "ГУАП" , id: -122496494),
-			FeedSource(name: "Профком" , id: -232453),
-			FeedSource(name: "Медиа" , id: -5515524),
-			FeedSource(name: "Спорт" , id: -138336798),
-			FeedSource(name: "Музгуап" , id: -66449391),
-			FeedSource(name: "Гараж" , id: -149885408),
-			FeedSource(name: "КВН" , id: -9187),
-		] : SANews.shared.sources
 		return from.map { item in
 			SAFeedElement.fromVK(item)
 		}
