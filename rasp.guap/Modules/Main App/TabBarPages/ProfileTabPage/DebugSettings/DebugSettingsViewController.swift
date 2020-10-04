48c06a6eac0849a7f4a8f18c956eb8dfa80f7cc0
Kovalev Kirill <kirilkovalev@ya.ru>
2020-10-04 13:53:07 +0300

    debug settings

--- rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/DebugSettings/DebugSettingsViewController.swift
+++ rasp.guap/Modules/Main App/TabBarPages/ProfileTabPage/DebugSettings/DebugSettingsViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class DebugSettingsViewController: UIViewController {
-	let rootView = DebugSettingsView()
-	override func loadView() {
-		self.view = rootView
-	}
+class DebugSettingsViewController: ModalViewController <DebugSettingsView> {
+
 	override func viewDidLoad() {
-		self.rootView.textField.text = Logger.log
+//		self.rootView.textField.text = Logger.log
+		self.content.textField.text = Logger.log
+		self.content.oldimetable.toggle.isOn = AppSettings.isOldTimetableEnabled
+		self.content.oldimetable.toggle.addTarget(self, action: #selector(self.setOldTimetable), for: .valueChanged)
+	}
+	@objc private func setOldTimetable(){
+		AppSettings.isOldTimetableEnabled = self.content.oldimetable.toggle.isOn
+		Logger.print(from: "", "ВСЕ ОК, отчеты отклоню, оно не вылетает, а закрывается")
+		exit(0)
 	}
 }
