bad8eb123777c8166228cc52d9e5c2c69d4b658e
Kovalev Kirill <kirilkovalev@ya.ru>
2020-10-07 10:58:51 +0300

    Refresh image in assets

--- rasp.guap/Constants/Assets.swift
+++ rasp.guap/Constants/Assets.swift
@@ -125,6 +125,7 @@ internal enum Asset {
     internal static let add = ImageAsset(name: "add")
     internal static let cancelCircle = ImageAsset(name: "cancel_circle")
     internal static let checkCircle = ImageAsset(name: "check_circle")
+    internal static let reload = ImageAsset(name: "reload")
     internal static let searchDropdown = ImageAsset(name: "searchDropdown")
     internal static let searchLense = ImageAsset(name: "searchLense")
     internal static let vkLogo = ImageAsset(name: "vkLogo")
