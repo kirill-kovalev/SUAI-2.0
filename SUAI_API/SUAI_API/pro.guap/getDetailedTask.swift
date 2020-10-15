39641e144a92d5196241d85863262f1b33dd8747
Kovalev Kirill <kirilkovalev@ya.ru>
2020-10-15 12:56:11 +0300

    Pro Guap task caching

--- SUAI_API/SUAI_API/pro.guap/getDetailedTask.swift
+++ SUAI_API/SUAI_API/pro.guap/getDetailedTask.swift
@@ -9,16 +9,49 @@
 import Foundation
 
 public extension ProGuap{
+	
+	fileprivate var defaultsKeyID:String {"ProGuapTaskCacheID"}
+	fileprivate var defaultsKeyDate:String {"ProGuapTaskCacheDate"}
+	
+	fileprivate func saveToCache(detailedTask task:ProTask.Detailed){
+		print("CACHE: saving task \(task.id)")
+		if let encoded = try? JSONEncoder().encode(task){
+			UserDefaults.standard.setValue(encoded, forKey: defaultsKeyID+task.id)
+			UserDefaults.standard.setValue(Date(), forKey: defaultsKeyDate+task.id)
+			print("CACHE: saved task \(task.id)")
+		}
+	}
+	fileprivate func getFromCache(detailedTaskID id:Int)->ProTask.Detailed? {
+		print("CACHE: loading task \(id)")
+		if let data = UserDefaults.standard.value(forKey: defaultsKeyID+"\(id)") as? Data,
+			let date = UserDefaults.standard.value(forKey: defaultsKeyDate+"\(id)") as? Date,
+			date.addingTimeInterval(3600*24*7) > Date(),
+			let decoded = try? JSONDecoder().decode(ProTask.Detailed.self, from: data)
+		{
+			print("CACHE: loaded task \(id)")
+			return decoded
+		}
+		return nil
+	}
+	
 	func getDetailedTask(id:Int) -> ProTask.Detailed?{
+		
+		if let fromCache = getFromCache(detailedTaskID: id){
+			return fromCache
+		}
+		
 		var headers:[String:String] = [:]
 		headers["Cookie"] = self.AUTH_COOKIE
 		headers["Sec-Fetch-Mode"] = "cors"
-
+		
 		let semaphore = DispatchSemaphore(value: 0)
 		var task:ProTask.Detailed? = nil
 		Self.POST("https://pro.guap.ru/get-student-task/\(id)", params: [:], headers: headers) { (data, resp, err) in
 			if let data = data {
 				task = self.decodeDetailedTasks(data)
+				if let task = task {
+					self.saveToCache(detailedTask: task)
+				}
 			}
 			
 			semaphore.signal()
